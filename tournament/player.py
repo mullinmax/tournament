b76c55cef571435d7d0ea18eb5475788fe2db5a9
mullinmax <mullinmax@gmail.com>
2020-02-11 08:08:54 -0500

    better directory cleanup

--- tournament/player.py
+++ tournament/player.py
@@ -13,19 +13,25 @@ class player():
         self.author = author
 
     def take_turn(self, data = None):
-        if os.path.exists('./output.txt'):
-            os.remove('./output.txt')
-        if os.path.exists('./input.txt'):
-            os.remove('./input.txt')
+        self.clean_directory()
         if data is not None:
             f = open('./input.txt', 'w')
             f.write(data)
             os.system('cat ./input.txt | ' + self.path +' > ./output.txt')
             f = open('./output.txt', 'r')
+            lines = f.readlines()
         else:
             os.system(self.path +' > ./output.txt')
             f = open('./output.txt', 'r')
-        return f.readlines()
+            lines = f.readlines()
+        self.clean_directory()
+        return lines
+
+    def clean_directory(self):
+        if os.path.exists('./output.txt'):
+            os.remove('./output.txt')
+        if os.path.exists('./input.txt'):
+            os.remove('./input.txt')
 
     def set_score(self, n):
         self.score = n
